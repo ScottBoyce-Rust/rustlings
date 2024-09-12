c1dba0c7eda14d20e94cf9b26d348d6985bc42e3
Scott Boyce <seboyce@usgs.gov>
2024-09-12 07:22:07 -0700

    quiz 2 complete

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,17 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut v: Vec<String> = Vec::with_capacity(input.len());
+        for (s, cmd) in input {
+            v.push(match cmd {
+                Command::Uppercase => s.to_uppercase(),
+                Command::Trim => s.trim().to_string(),
+                Command::Append(n) => format!("{s}{}", "bar".repeat(n)),
+            });
+        }
+        v
+    }
 }
 
 fn main() {
@@ -37,7 +47,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
